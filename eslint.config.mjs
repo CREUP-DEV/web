d6564f6871304ae71ee202904221e54907d9edaa
ComicIvans <isalidocobo@gmail.com>
2025-10-20 19:25:45 +0200

    Prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
-// Use eslint-config-prettier to disable formatting-related ESLint rules
-// and let Prettier handle formatting separately (via the editor/CI).
-import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-// Combine Nuxt's defaults with Prettier compatibility.
-export default withNuxt([eslintConfigPrettier])
+export default withNuxt([eslintPluginPrettierRecommended])
